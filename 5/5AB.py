f6d6f4758f2541ed3bf2a66b4ef1c2aecd290bb8
pascal <e.a.hildebrand@student.tudelft.nl>
2020-12-05 10:47:40 +0100

    just plane fun

---
+++ 5/5AB.py
@@ -0,0 +1,5 @@
+# Exercise A: build a binary number out of the seating instructions, then retrieve the seat id from that
+print(max(a*8+b for a, b in ((map(lambda x: int(x, 2), (string[:7], string[7:]))) for string in list(("".join("0" if s in ["F","L"] else "1" for s in line[:-1])) for line in open('seats.txt')))))
+
+# Exercise B: (min and max values 42, 922 found by calling sorted on the whole)
+print(set(range(42,922)) - set(a*8+b for a, b in ((map(lambda x: int(x, 2), (string[:7], string[7:]))) for string in list(("".join("0" if s in ["F","L"] else "1" for s in line[:-1])) for line in open('seats.txt')))))
