2de111cfcfe1b8af98b59628ad4b2b4d7acfafaa
pascal <e.a.hildebrand@student.tudelft.nl>
2020-12-06 11:48:08 +0100

    make it slightly prettier

--- 6/6AB.py
+++ 6/6AB.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 # Exercise A
-print(sum(len(set(str.replace("\n",""))) for str in open("questions.txt").read().split("\n\n")))
+print(sum(len(set(str) - {'\n'}) for str in open("questions.txt").read().split("\n\n")))
 
 # Exercise B
 print(sum(map(lambda c: len(list(c[v] for v in c if (c[v]>c['\n']))), (Counter(str) for str in open("questions.txt").read()[:-1].split("\n\n")))))
